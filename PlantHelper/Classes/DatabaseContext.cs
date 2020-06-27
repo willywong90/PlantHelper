7dec38bad0637f2d8bf76b813e58042ba3dc304a
Willy W <will.wong90@gmail.com>
2020-06-26 20:14:07 -0700

    Create DB context

---
+++ PlantHelper/Classes/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlantHelper.Classes
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Plant> Plants { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=data.db");
+        }
+    }
+}
